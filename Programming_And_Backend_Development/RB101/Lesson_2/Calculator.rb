772be13f889eb695fb8765ef350d7d86818470d9
Pedro Lima <pedrogdcl@gmail.com>
2023-12-08 01:30:53 +0000

    Wokring on Calculator.rb file

---
+++ Programming_And_Backend_Development/RB101/Lesson_2/Calculator.rb
@@ -0,0 +1,27 @@
+#Create a calculator program that takes two numbers provided by the user and performs an operation chosen by the user
+
+#Welcome user
+puts "Welcome! This program will take two numbers you'll provide, ask waht operation you would like to perform, and then return the result."
+
+#Get two numbers
+print "First number: "
+number1 = gets.chomp
+print "Second number: "
+number2 = gets.chomp
+
+#Select operation
+puts "Would you like to add, subtract, multiply, or divide?"
+operation = gets.chomp
+
+#Perform and print result
+if operation == "add"
+  result = number1.to_i + number2.to_i
+elsif operation == "subtract"
+  result = number1.to_i - number2.to_i
+elsif operation == "multiply"
+  result = number1.to_i * number2.to_i
+elsif operation == "divide"
+  result = number1.to_f / number2.to_f
+end
+
+puts result
