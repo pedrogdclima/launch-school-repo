2635072c2418700ab171cc0b25102d8b2680f831
Pedro Lima <pedrogdcl@gmail.com>
2023-12-31 15:35:08 +0000

    Used Rubocop to fix some issues with Calculator.rb

--- Programming_And_Backend_Development/RB101/Lesson_2/Calculator.rb
+++ Programming_And_Backend_Development/RB101/Lesson_2/Calculator.rb
@@ -1,19 +1,19 @@
-#Create a calculator program that takes two numbers provided by the user and performs an operation chosen by the user
+# Create a calculator program that takes two numbers provided by the user and performs an operation chosen by the user
 
-#Welcome user
+# Welcome user
 puts "Welcome! This program will take two numbers you'll provide, ask waht operation you would like to perform, and then return the result."
 
-#Get two numbers
+# Get two numbers
 print "First number: "
 number1 = gets.chomp
 print "Second number: "
 number2 = gets.chomp
 
-#Select operation
+# Select operation
 puts "Would you like to add, subtract, multiply, or divide?"
 operation = gets.chomp
 
-#Perform and print result
+# Perform and print result
 if operation == "add"
   result = number1.to_i + number2.to_i
 elsif operation == "subtract"
