7bbdc3c853c1493283805440068569eb70203eb7
Pedro Lima <pedrogdcl@gmail.com>
2024-01-10 13:38:13 +0000

    Add features to Calc app

--- Programming_And_Backend_Development/RB101/Lesson_2/Calculator.rb
+++ Programming_And_Backend_Development/RB101/Lesson_2/Calculator.rb
@@ -1,4 +1,5 @@
-# Create a calculator program that takes two numbers provided by the user and performs an operation chosen by the user
+# Create a calculator program that takes two numbers provided by the user
+#and performs an operation chosen by the user
 
 # Welcome user
 def prompt(message)
@@ -9,7 +10,18 @@ def valid_number(num)
   num.to_i != 0
 end
 
-prompt("Welcome! This program will take two numbers you'll provide, ask waht operation you would like to perform, and then return the result.")
+prompt("Welcome! This program will take two numbers you'll provide, ask what operation you would like to perform, and then return the result.")
+
+sleep 1
+
+prompt "But before we start, what is your name?"
+
+username = gets.chomp
+
+prompt "Hi #{username}! Let's get started"
+
+sleep 1
+
 loop do
   # Get two numbers
   number1 = nil
@@ -18,6 +30,7 @@ loop do
     number1 = gets.chomp
     break if valid_number(number1)
     prompt("That is not a valid response. Please try again.")
+    sleep 1
   end
   
   number2 = nil
@@ -26,26 +39,39 @@ loop do
     number2 = gets.chomp
     break if valid_number(number2)
     prompt("That is not a valid response. Please try again.")
+    sleep 1
   end
   
   # Select operation
-  prompt("Would you like to add, subtract, multiply, or divide?")
+  operator_prompt = <<-TXT
+    Would you like to:
+    1 => Add
+    2 => Subtract
+    3 => Multiply
+    4 => Divide
+  TXT
+  
+  prompt operator_prompt
   operation = gets.chomp
   
   # Perform and print result
   result = case operation
-          when "add"
+          when "1"
             number1.to_i + number2.to_i
-          when "subtract"
+          when "2"
             number1.to_i - number2.to_i
-          when "multiply"
+          when "3"
             number1.to_i * number2.to_i
-          when "divide"
+          when "4"
             number1.to_f / number2.to_f
           end
   
   prompt("The result is #{result}!")
   
+  sleep 2
+  
   prompt("Would you like to perform another calculation?")
   break unless gets.downcase.start_with?("y")
 end
+
+prompt "Thanks for using the Calculator app #{username}!"
