c7f8bb3849b3592dab0a4a6e4c1281fc67becbaa
Pedro Lima <pedrogdcl@gmail.com>
2024-04-18 16:47:35 +0000

    Added rubocop file and tictactoe file

---
+++ Programming_And_Backend_Development/RB110/Lesson_3/tictactoe.rb
@@ -0,0 +1,17 @@
+def display_board()
+  puts ""
+  puts "   |   |"
+  puts " X | X | O"
+  puts "   |   |"
+  puts "---|---|---"
+  puts "   |   |"
+  puts " X |   |"
+  puts "   |   |"
+  puts "---|---|---"
+  puts "   |   |"
+  puts "   |   |"
+  puts "   |   |"
+  puts ""
+end
+
+display_board
