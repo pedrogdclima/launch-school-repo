8f87a7ad376aa5098041087a8a6292cca7124853
Pedro Lima <pedrogdcl@gmail.com>
2024-12-01 22:46:44 -0500

    Meetups challenge solved

--- Programming_And_Backend_Development/Exercises/RubyJavaScript_Challenges/Medium/meetups.rb
+++ Programming_And_Backend_Development/Exercises/RubyJavaScript_Challenges/Medium/meetups.rb
@@ -1,13 +1,52 @@
-# Problem
-# - 
+require 'date'
 
-# Examples/Test Cases
-# - 
+class Meetup
+  @@position_hash = {
+    'first' => 0,
+    'second' => 1,
+    'third' => 2,
+    'fourth' => 3,
+    'fifth' => 4
+  }
 
-# Data
-# - 
+  def initialize(year, month)
+    @year = year
+    @month = month
+  end
 
-# Algorithm
-# - 
+  def day(weekday, position)
+    @weekday = weekday.downcase
+    @position = position.downcase
+    find_day
+  end
 
-# Code
+  def find_day
+    possible_dates = weekdays_in_month
+    which_day(possible_dates)
+  rescue NoMethodError
+    puts "Error", "Invalid input => #{@weekday}"
+  rescue ArgumentError
+    puts "Error", "Invalid input => #{@position}"
+  end
+
+  def which_day(possible_dates)
+    if @@position_hash.key?(@position)
+      possible_dates[@@position_hash[@position]]
+    elsif @position == 'last'
+      possible_dates[-1]
+    elsif @position == 'teenth'
+      possible_dates.find { |date| (13..19).include?(date.mday) }
+    else
+      raise ArgumentError
+    end
+  end
+
+  def weekdays_in_month
+    possible_dates = []
+    last_day = Date.new(@year, @month, -1)
+    Date.new(@year, @month).step(last_day) do |date|
+      possible_dates << date if date.send("#{@weekday}?")
+    end
+    possible_dates
+  end
+end
