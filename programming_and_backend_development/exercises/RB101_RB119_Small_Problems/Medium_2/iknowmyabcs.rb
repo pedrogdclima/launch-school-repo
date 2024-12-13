1219473506b8cbba26aaca598a551b893fb04b55
Pedro Lima <pedrogdcl@gmail.com>
2024-12-13 17:07:33 -0500

    RB101-119 exercise solved

--- programming_and_backend_development/exercises/RB101_RB119_Small_Problems/Medium_2/iknowmyabcs.rb
+++ programming_and_backend_development/exercises/RB101_RB119_Small_Problems/Medium_2/iknowmyabcs.rb
@@ -13,3 +13,26 @@ Write a method that returns true if the word passed in as an argument can be
 spelled from this set of blocks, false otherwise.
 =end
 
+LETTER_BLOCKS = [
+  %w(B O), %w(X K), %w(D Q), %w(C P), %w(N A), %w(G T), %w(R E), %w(F S),
+  %w(J W), %w(H U), %w(V I), %w(L Y), %w(Z M)
+  ]
+
+def block_word?(string)
+  input = string.upcase
+  LETTER_BLOCKS.each do |block|
+    counter = 0
+    block.each do |letter|
+      counter += 1 if input.include?(letter)
+      return false if input.count(letter) > 1
+    end
+    return false if counter == 2
+  end
+  true
+end
+
+p block_word?('BATCH') == true
+p block_word?('BUTCH') == false
+p block_word?('jest') == true
+p block_word?('apples') == false
+p block_word?('Baby') == false
