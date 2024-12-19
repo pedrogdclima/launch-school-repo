2a501801d4c98ca950e05947887fa06b4b0c4de6
Pedro Lima <pedrogdcl@gmail.com>
2024-12-19 14:23:31 -0500

    Solved another RB110 exercise

---
+++ programming_and_backend_development/exercises/RB101_RB119_Small_Problems/Medium_2/matching_parentheses.rb
@@ -0,0 +1,25 @@
+=begin
+Write a method that takes a string as an argument, and returns true if all parentheses in the string are properly balanced, false otherwise. To be properly balanced, parentheses must occur in matching '(' and ')' pairs.
+
+Note that balanced pairs must each start with a (, not a ).
+=end
+
+def balanced?(string)
+  counter = 0
+  string.each_char do |char|
+    next if !('('..')').include?(char)
+    counter += (char == '(' ? 1 : -1)
+    return false if counter < 0
+  end
+  counter.zero?
+end
+
+p balanced?('What (is) this?') == true
+p balanced?('What is) this?') == false
+p balanced?('What (is this?') == false
+p balanced?('((What) (is this))?') == true
+p balanced?('((What)) (is this))?') == false
+p balanced?('Hey!') == true
+p balanced?(')Hey!(') == false
+p balanced?('What ((is))) up(') == false
+p balanced?('What ())(is() up') == false
