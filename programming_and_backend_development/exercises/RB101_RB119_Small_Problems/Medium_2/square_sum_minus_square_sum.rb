b978b59cf20a4eb695ab77a20dcd4d488540b8d8
Pedro Lima <pedrogdcl@gmail.com>
2024-12-26 13:37:09 -0500

    Sum Square - Square Sum RB110 exercise completed

---
+++ programming_and_backend_development/exercises/RB101_RB119_Small_Problems/Medium_2/square_sum_minus_square_sum.rb
@@ -0,0 +1,20 @@
+=begin
+Write a method that computes the difference between the square of the sum of the first n positive integers and the sum of the squares of the first n positive integers.
+=end
+
+def sum_square_difference(integer)
+  first_sum = 0
+  second_sum = 0
+  1.upto(integer) do |i|
+    first_sum += i
+    second_sum += (i**2)
+  end
+  first_sum = first_sum ** 2
+  first_sum - second_sum
+end
+
+p sum_square_difference(3) == 22
+   # -> (1 + 2 + 3)**2 - (1**2 + 2**2 + 3**2)
+p sum_square_difference(10) == 2640
+p sum_square_difference(1) == 0
+p sum_square_difference(100) == 25164150
