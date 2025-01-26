d15d77f548ddeecaecaec1669be9cfe82c4d7dd2
Pedro <pedrogdcl@gmail.com>
2025-01-26 11:22:14 -0500

    New counter_server.rb

--- programming_and_backend_development/RB175/counter_server.rb
+++ programming_and_backend_development/RB175/counter_server.rb
@@ -5,7 +5,7 @@ server = TCPServer.new("localhost", 3030)
 def parse_request(request_line)
   method, path_and_params, scheme = request_line.split
   path, params = path_and_params.split("?")
-  params_hash = params.split("&").each_with_object({}) do |nv, hash|
+  params_hash = (params || "").split("&").each_with_object({}) do |nv, hash|
     name, value = nv.split("=")
     hash[name] = value
   end
@@ -23,16 +23,13 @@ loop do
   client.puts "Content-Type: text/html\r\n\r\n"
 
   method, path, params_hash, scheme = parse_request(request_line)
-  dice = params_hash["dice"].to_i
-  sides = params_hash["sides"].to_i
+  counter = params_hash["counter"].to_i
 
-  client.puts "<html>", "<body>", "<p>"
-  client.puts "The #{dice} rolls are:\r\n"
-  client.puts "<ul>"
-  dice.times do
-    client.puts "<li>#{rand(sides) + 1}</li>"
-  end
-  client.puts "</ul></p>"
+  client.puts "<html>", "<body>"
+  client.puts "<h2>Counter</h2>"
+  client.puts "<p>#{counter}</p>"
+  client.puts "<p><a href='?counter=#{counter + 1}'>Plus one</a></p>"
+  client.puts "<p><a href='?counter=#{counter - 1}'>Minus one</a></p>"
   client.puts "</body>", "</html>"
   client.close
 end
