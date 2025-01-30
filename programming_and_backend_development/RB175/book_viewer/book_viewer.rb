7fa6957f6449648ca21c69889217bc644c6bc04d
Pedro Lima <pedrogdcl@gmail.com>
2025-01-30 16:27:38 -0500

    Add search function to book_viewer

--- programming_and_backend_development/RB175/book_viewer/book_viewer.rb
+++ programming_and_backend_development/RB175/book_viewer/book_viewer.rb
@@ -2,15 +2,75 @@ require "sinatra"
 require "sinatra/reloader"
 require "tilt/erubi"
 
-get "/" do
+before do
+
   @list_of_titles = File.readlines("data/toc.txt")
-  @title = "Working Title"
-  erb :home
+
 end
 
-get "/chapter/1" do
-  @list_of_titles = File.readlines("data/toc.txt")
-  @title = "Chapter 1"
-  @chapter_text = File.read("data/chp1.txt")
+not_found do
+
+  redirect "/"
+
+end
+
+get "/" do
+
+  @title = "The Adventures of Sherlock Holmes"
+  erb :home
+
+end
+
+get "/chapter/:num" do
+
+  number = params[:num].to_i
+  chapter_name = @list_of_titles[number - 1]
+
+  redirect "/" unless (1..@list_of_titles.size).cover? number
+
+  @title = "Chapter #{number}: #{chapter_name}"
+  @chapter_text = File.read("data/chp#{number}.txt")
   erb :chapter
+
+end
+
+def each_chapter
+  @list_of_titles.each_with_index do |name, index|
+    number = index + 1
+    contents = File.read("data/chp#{number}.txt")
+    yield number, name, contents
+  end
+end
+
+def chapters_matching(query)
+  results = []
+
+  return results unless query
+
+  each_chapter do |number, name, contents|
+    matches = {}
+    contents.split("\n\n").each_with_index do |paragraph, index|
+      matches[index] = paragraph if paragraph.include?(query)
+    end
+    results << {number: number, name: name, paragraphs: matches} if matches.any?
+  end
+
+  results
+end
+
+get "/search" do
+  @results = chapters_matching(params[:query])
+  erb :search
+end
+
+helpers do
+  def in_paragraphs(text)
+    text.split("\n\n").each_with_index.map do |line, index|
+      "<p id=paragraph#{index}>#{line}</p>"
+    end.join
+  end
+
+  def highlight(text, term)
+    text.gsub(term, %(<strong>#{term}</strong>))
+  end
 end
