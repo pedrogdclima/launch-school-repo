61e99148630b7556c2346313de23c76a3b45cbdf
Pedro Lima <pedrogdcl@gmail.com>
2025-10-06 11:33:27 -0400

    Complete search autocomplete project

--- front_end_development/JS235/search_autocomplete_project/autocomplete_node/public/javascripts/autocomplete.js
+++ front_end_development/JS235/search_autocomplete_project/autocomplete_node/public/javascripts/autocomplete.js
@@ -1,15 +1,43 @@
 // const { urlencoded } = require("express");
+import debounce from './debounce.js';
 
-const Autocomplete = {
+class Autocomplete {
+  constructor(input, url) {
+    this.input = input;
+    this.url = url;
 
-  wrapInput: function() {
+    this.listUI = null;
+    this.overlay = null;
+
+    this.visible = false;
+    this.matches = [];
+    this.bestMatchIndex = null;
+    this.selectedIndex = null;
+    this.previousValue = null;
+
+    this.wrapInput();
+    this.createUI();
+
+    this.valueChanged = debounce(this.valueChanged.bind(this), 300);
+
+    this.bindEvents();
+    // this.reset();
+  }
+
+  bindEvents() {
+    this.input.addEventListener('input', this.valueChanged);
+    this.input.addEventListener('keydown', this.handleKeydown.bind(this));
+    this.listUI.addEventListener('mousedown', this.handleMousedown.bind(this));
+  }
+
+  wrapInput() {
     let wrapper = document.createElement('div');
     wrapper.classList.add('autocomplete-wrapper');
     this.input.parentNode.appendChild(wrapper);
     wrapper.appendChild(this.input);
-  },
+  }
 
-  createUI: function() {
+  createUI() {
     let listUI = document.createElement('ul');
     listUI.classList.add('autocomplete-ui');
     this.input.parentNode.appendChild(listUI);
@@ -21,37 +49,9 @@ const Autocomplete = {
 
     this.input.parentNode.appendChild(overlay);
     this.overlay = overlay;
-  },
+  }
 
-  fetchMatches: async function(query) {
-    let response = await fetch(`${this.url}${encodeURIComponent(query)}`);
-    let data = await response.json();
-    return data;
-  },
-
-  bindEvents: function() {
-    this.input.addEventListener('input', this.valueChanged.bind(this));
-  },
-
-  valueChanged: async function() {
-    let value = this.input.value;
-
-    if (value.length > 0) {
-      this.matches = await this.fetchMatches(value);
-      this.visible = true;
-      this.bestMatchIndex = 0;
-      this.draw();
-    } else {
-      this.reset();
-    }
-  },
-
-  generateOverlayContent: function(value, match) {
-    let end = match.name.slice(value.length);
-    return value + end;
-  },
-
-  draw: function() {
+  draw() {
     while (this.listUI.lastChild) {
       this.listUI.removeChild(this.listUI.lastChild);
     }
@@ -71,42 +71,103 @@ const Autocomplete = {
       this.overlay.textContent = '';
     }
 
-    this.matches.forEach(match => {
+    this.matches.forEach((match, index) => {
       let li = document.createElement('li');
       li.classList.add('autocomplete-ui-choice');
+
+      if (index === this.selectedIndex) {
+        li.classList.add('selected');
+        this.input.value = match.name;
+      }
 
       li.textContent = match.name;
       this.listUI.appendChild(li);
     })
-  },
+  }
 
-  reset: function() {
+  generateOverlayContent(value, match) {
+    let end = match.name.slice(value.length);
+    return value + end;
+  }
+
+  async fetchMatches(query) {
+    let response = await fetch(`${this.url}${encodeURIComponent(query)}`);
+    let data = await response.json();
+    return data;
+  }
+
+  handleKeydown(event) {
+    switch (event.key) {
+      case 'ArrowDown':
+        event.preventDefault();
+        if (
+          this.selectedIndex === null || this.selectedIndex === this.matches.length - 1
+        ) {
+          this.selectedIndex = 0;
+        } else {
+          this.selectedIndex += 1;
+        }
+        this.bestMatchIndex = null;
+        this.draw();
+        break;
+      case 'ArrowUp':
+        event.preventDefault();
+        if (this.selectedIndex === null || this.selectedIndex === 0) {
+          this.selectedIndex = this.matches.length - 1;
+        } else {
+          this.selectedIndex -= 1;
+        }
+        this.bestMatchIndex = null;
+        this.draw();
+        break;
+      case 'Tab':
+        if (this.bestMatchIndex !== null && this.matches.length !== 0) {
+          this.input.value = this.matches[this.bestMatchIndex].name;
+          event.preventDefault();
+        }
+        this.reset();
+        break;
+      case 'Enter':
+        this.reset();
+        break;
+      case 'Escape':
+        this.input.value = this.previousValue;
+        break;
+    }
+  }
+
+  handleMousedown(event) {
+    this.input.value = event.target.textContent;
+    this.reset();
+  }
+
+  reset() {
     this.visible = false;
     this.matches = [];
     this.bestMatchIndex = null;
+    this.selectedIndex = null;
+    this.previousValue = null;
 
     this.draw();
-  },
+  }
 
-  init: function() {
-    this.input = document.querySelector('input');
-    this.url = '/countries?matching=';
+  async valueChanged() {
+    let value = this.input.value;
+    this.previousValue = value;
 
-    this.listUI = null;
-    this.overlay = null;
-
-    this.visible = false;
-    this.matches = [];
-    this.bestMatchIndex = null;
-
-    this.wrapInput();
-    this.createUI();
-
-    this.bindEvents();
-    this.reset();
-  },
-};
+    if (value.length > 0) {
+      this.matches = await this.fetchMatches(value);
+      this.visible = true;
+      this.bestMatchIndex = 0;
+      this.selectedIndex = null;
+      this.draw();
+    } else {
+      this.reset();
+    }
+  }
+}
 
 document.addEventListener('DOMContentLoaded', () => {
-  Autocomplete.init();
+  let input = document.querySelector('input');
+  let countryAutocomplete = new Autocomplete(input, '/countries?matching=');
 });
